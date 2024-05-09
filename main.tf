fb61c82b1e004b2b94c66b87f5c44341024de590
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-05-09 11:57:36 -0300

    chore: prevent_deletion_if_contains_resources

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name      = "devops-rocketseat-rcmonteiro-hmg"
-  location  = "brazilsouth"
+  name     = "devops-rocketseat-rcmonteiro-hmg"
+  location = "brazilsouth"
+  tags = {
+    IaC = true
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rocketseat-rcmonteiro-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+
   tags = {
     IaC = true
   }
